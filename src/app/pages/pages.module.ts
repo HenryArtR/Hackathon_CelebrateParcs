15ef6646cae5b98186cd9d2c38bf708335ca136c
HenryAr <henryarturorivera@gmail.com>
2022-05-19 22:13:58 +0200

    bienvenida page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -6,6 +7,7 @@ import { MaterialModule } from '../material/material.module';
 import { ComponentsModule } from '../components/components.module';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -17,6 +19,8 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
+    RouterModule,
+    ReactiveFormsModule,
     MaterialModule,
     ComponentsModule,
     SharedModule
--- src/app/pages/welcome-page/welcome-page.component.ts
+++ src/app/pages/welcome-page/welcome-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-welcome-page',
@@ -9,7 +10,22 @@ export class WelcomePageComponent implements OnInit {
 
   
 
-  constructor() { }
+  loginForm: FormGroup;
+  
+
+  constructor(fb: FormBuilder) {
+    this.loginForm = fb.group(
+      {email: ['', Validators.required]}
+    );
+  }
+
+  getErrorMessage() {
+    if (this.loginForm.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.loginForm.hasError('email') ? 'Not a valid email' : '';
+  }
 
   ngOnInit(): void {
   }
